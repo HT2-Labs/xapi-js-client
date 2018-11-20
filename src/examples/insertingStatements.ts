bec44b2f0c56926c909ba0e1c4d7ca13f4af6fb4
Ryan Smith <0ryansmith1994@gmail.com>
2018-11-20 13:08:06 +0000

    fix(examples): Fixes linting error in example.

--- src/examples/insertingStatements.ts
+++ src/examples/insertingStatements.ts
@@ -2,22 +2,17 @@ import createBasicAuthToken from '../statementUtils/createBasicAuthToken';
 import insertStatements from '../statementUtils/insertStatements';
 import actionOnSiteActivity from './actionOnSiteActivity';
 
-const main = async () => {
-  try {
-    await insertStatements({
-      statements: [actionOnSiteActivity],
-      endpoint: 'https://example.org/lrs/xAPI',
-      authToken: createBasicAuthToken({
-        key: 'demo-key',
-        secret: 'demo-secret',
-      }),
-    });
-    // tslint:disable-next-line:no-console
-    console.log('Successfully inserted statements');
-  } catch {
-    // tslint:disable-next-line:no-console
-    console.log('Failed inserting statements');
-  }
-};
-
-main();
+insertStatements({
+  statements: [actionOnSiteActivity],
+  endpoint: 'https://example.org/lrs/xAPI',
+  authToken: createBasicAuthToken({
+    key: 'demo-key',
+    secret: 'demo-secret',
+  }),
+}).then(() => {
+  // tslint:disable-next-line:no-console
+  console.log('Successfully inserted statements');
+}).catch(() => {
+  // tslint:disable-next-line:no-console
+  console.log('Failed inserting statements');
+});
