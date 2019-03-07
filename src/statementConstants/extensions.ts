d7059db42bf2d9df23724c2f909e8fcd0ae8d1da
Ryan Smith <0ryansmith1994@gmail.com>
2019-03-07 13:45:44 +0000

    feat(Extensions): Adds useful extensions.

--- src/statementConstants/extensions.ts
+++ src/statementConstants/extensions.ts
@@ -5,3 +5,13 @@ export const booleanResponseExtension = `${baseUrl}/boolean-response`;
 export const choiceResponseExtension = `${baseUrl}/choice-response`;
 export const selectionResponseExtension = `${baseUrl}/selection-response`;
 export const numericResponseExtension = `${baseUrl}/numeric-response`;
+
+export const cityExtension = `${baseUrl}/city`;
+export const stateExtension = `${baseUrl}/state`;
+export const assignmentDateExtension = `${baseUrl}/assignment-date`;
+export const registrationDateExtension = `${baseUrl}/registration-date`;
+
+export const plannedStartTime = 'http://id.tincanapi.com/extension/planned-start-time';
+export const plannedEndTime = 'http://id.tincanapi.com/extension/planned-end-time';
+export const plannedDuration = 'http://id.tincanapi.com/extension/planned-duration';
+export const trainingProvider = 'http://id.tincanapi.com/extension/training-provider';
